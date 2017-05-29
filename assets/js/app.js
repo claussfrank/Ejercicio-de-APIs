a643c515e7b1ce2888e1c1ce9d7588c1bf0bc7f9
Clauss Franco <you@example.com>
2017-05-28 23:40:15 -0500

    Primera Funcion a medias

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+var tituloMensaje =  document.getElementById("tituloMensaje");
+var textMensaje = document.getElementById("textMensaje");
+var enviarMensaje = document.getElementById("mensajeSend");
+
+enviarMensaje.addEventListener("click", mostarPublicacion);
+
+function mostarPublicacion(){
+    var publicar = document.getElementById("publicar");
+    var nuevaPublicacion = document.createElement("div");
+    var nuevoTexto = document.createElement("p");
+    var nuevoTitulo = document.createElement("h3");
+    
+    nuevaPublicacion.className ="card-panel" + " " + "hoverable";
+    
+    nuevoTitulo.innerHTML = tituloMensaje.value;
+    nuevoTexto.innerHTML = textMensaje.value;
+    nuevaPublicacion.appendChild(nuevoTitulo);
+    nuevaPublicacion.appendChild(nuevoTexto);
+    publicar.appendChild(nuevaPublicacion);
+}
